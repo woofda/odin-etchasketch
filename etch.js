c9341bc7b803c713ebfce822c4e90a5ce3c81b56
woofda <130379195+woofda@users.noreply.github.com>
2023-07-18 10:13:22 -0500

    Add coloring style functions

--- etch.js
+++ etch.js
@@ -1,18 +1,41 @@
 let area = document.querySelector('.container');
 
-// Creates single square element
+// Coloring functions
+let shades = [''];
+for(let i = 255; i > 27; i -= 28) {
+    shades.push(`rgb(${i}, ${i}, ${i})`);
+}
+
+function standardColoring(square) {
+    square.style.backgroundColor = "#5f5f5f";
+}
+
+function randomColoring(square) {
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    square.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+}
+
+function gradedColoring(square) {
+    let shade = shades.indexOf(square.style.backgroundColor);
+    if(shade < 9) {
+        square.style.backgroundColor = shades[shade + 1];
+    }
+}
+
+// Grid creation functions
 function createSquare() {
     const newSquare = document.createElement('div');
     newSquare.style.border = "1px solid black";
     newSquare.style.flex = "1";
     newSquare.addEventListener('mouseover', () => {
-        newSquare.style.backgroundColor = "#5f5f5f";
-    })
+        gradedColoring(newSquare);
+    });
 
     return newSquare;
 }
 
-// Creates row container filled with square elements
 function createRow(length) {
     const rowContainer = document.createElement('div');
     rowContainer.style.display = "flex";
@@ -25,7 +48,6 @@ function createRow(length) {
     return rowContainer;
 }
 
-// Fills container with filled row elements
 function createGrid(sideLength) {
     for(let i = 0; i < sideLength; i++) {
         area.appendChild(createRow(sideLength));
